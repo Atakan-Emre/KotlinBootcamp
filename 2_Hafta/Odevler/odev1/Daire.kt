8daa9860e89e240a065d12f42272c08d4cf59eb5
Atakan Emre <satakanemre@gmail.com>
2023-07-27 22:59:32 +0300

    2.hafta odeb 1 tamamlandı

--- 2_Hafta/Odevler/odev1/Daire.kt
+++
@@ -1,9 +0,0 @@
-class Daire(val yaricap: Double): GeometrikSekil() {
-    override fun alanHesapla(): Double {
-        return Math.PI * yaricap * yaricap
-    }
-
-    override fun cevreHesapla(): Double {
-        return 2 * Math.PI * yaricap
-    }
-}
--- 2_Hafta/Odevler/odev1/GeometrikSekil.kt
+++
@@ -1,9 +0,0 @@
-open class GeometrikSekil {
-    open fun alanHesapla(): Double {
-        return 0.0
-    }
-
-    open fun cevreHesapla(): Double {
-        return 0.0
-    }
-}
