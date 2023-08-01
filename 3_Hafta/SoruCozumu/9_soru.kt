850ed8c948955cb4b78dccc3e600d2f6d4ab9b71
Atakan-Emre <atakan21@outlook.com>
2023-08-01 21:53:51 +0300

    9.soru

--- 3_Hafta/SoruCozumu/9_soru.kt
+++ 3_Hafta/SoruCozumu/9_soru.kt
@@ -8,3 +8,19 @@ fun main() {
     val sonuc = elemanlariCikar(set1, set2)
     println("Sonuc: $sonuc")
 }
+
+
+/*
+
+fun main() {
+    val set1 = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val set2 = setOf(1, 2, 3, 4, 5)
+    differenceSetOf(set1, set2)
+}
+
+private fun differenceSetOf(set1: Set<Int>, set2: Set<Int>) {
+    println(set1.minus(set2))
+}
+
+
+*/
