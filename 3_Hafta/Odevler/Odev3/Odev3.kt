1b79f18da98c1da6716e238bc2e75647ef4c0ea8
Atakan Emre <satakanemre@gmail.com>
2023-08-03 21:50:27 +0300

    odev3

---
+++ 3_Hafta/Odevler/Odev3/Odev3.kt
@@ -0,0 +1,19 @@
+// Q1: Classes have a special method that serves as a blueprint for creating objects of that class. What is the method called?
+// Cevap: A constructor
+// Bir sınıfın nesnelerini oluştururken kullanılan özel yöntem "constructor" olarak adlandırılır. 
+
+// Q2: Which of the following statements about interfaces and abstract classes is NOT correct?
+// Cevap: Interfaces and abstract classes can be instantiated directly.
+// Arabirimler ve soyut sınıflar doğrudan somutlaştırılamazlar.
+
+// Q3: Which of the following is NOT a Kotlin visibility modifier for properties, methods, etc.?
+// Cevap: nosubclass
+// Kotlin'de 'internal', 'protected', 'private' geçerli görünürlük değiştiricilerdir. 'nosubclass' böyle bir değiştirici değildir.
+
+// Q4: Consider this data class: data class Fish(val name: String, val species:String, val colors:String) Which of the following is NOT valid code to create and destructure a Fish object?
+// Cevap: val (name4, species4, colors4) = Fish("Harry", "halibut")
+// Bu kod parçacığı geçersizdir çünkü 'Fish' veri sınıfının tüm parametreleri gereklidir ve burada 'colors' parametresi eksiktir.
+
+// Q5: Let's say you own a zoo with lots of animals that all need to be taken care of. Which of the following would NOT be part of implementing caretaking?
+// Cevap: A data class for an entry in a feeding schedule.
+// Bir besleme programındaki bir girdi için veri sınıfı, hayvan bakımının doğrudan bir parçası olarak düşünülmez.
